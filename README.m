3079e7969e2dbb1f8b18c428b43e58034f75eca4
psegoviaaguilar69-source <psegoviaaguilar69@gmail.com>
2026-05-14 08:23:12 -0600

    Revise README with author and project information
    Updated README with new author names and project details.

--- README.m
+++ README.m
@@ -1,1 +1,9 @@
-# enmanuel123
+#Pedro Enmanuel Segovia Aguilar
+#Joseph Calderon
+#Programacion 2 "A"
+#14/05/2026
+
+
+probucto = int(input("ingresa el valor del probucto"))
+#descuento que optiene segun la suerte
+amarillo = 
